2c9402deee01d06ac18a8a9e47c1c5211aa61872
Liora Shutel <liorash2@gmail.com>
2020-07-01 17:11:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var generate_uid = require('./routes/generate_uid');
+var users = require('./routes/users');
+var customer = require('./routes/customer');
+
+var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/v1/users', users);
+app.use('/api/v1/generate_uid', generate_uid);
+app.use('/api/v1/customer', customer);
+module.exports = app;
